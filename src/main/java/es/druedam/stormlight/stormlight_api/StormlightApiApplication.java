1446b4c261cdb96de44fe015e417af7e52e3300d
DRuedaM <davidruedamartin26@gmail.com>
2024-08-13 10:03:07 +0200

    Primer comit

---
+++ src/main/java/es/druedam/stormlight/stormlight_api/StormlightApiApplication.java
@@ -0,0 +1,13 @@
+package es.druedam.stormlight.stormlight_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormlightApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormlightApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/druedam/stormlight/stormlight_api/StormlightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.druedam.stormlight.stormlight_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StormlightApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
